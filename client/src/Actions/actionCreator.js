cd6a08d0f57b0d55870826724ee476d5ba0d7690
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2022-08-11 10:37:11 -0300

    enviroment variables

--- client/src/Actions/actionCreator.js
+++ client/src/Actions/actionCreator.js
@@ -10,14 +10,15 @@ import {
     SET_LOADING_TRUE,
     SET_LOADING_FALSE,
     SET_ERROR_TRUE,
-    SET_ERROR_FALSE,
     SET_FUNCTION,
   } from  "./constantes.js";
+
+  const API = process.env.REACT_APP_API
 
 
 export function getActivities(){
     return function (dispatch){
-        return axios.get('http://localhost:3001/activities')
+        return axios.get(API + '/activities')
                     .then((res)=>{
                         dispatch({type: GET_ACTIVITIES, payload: res.data})
                     })
@@ -30,7 +31,7 @@ export function getActivities(){
 
 export  function getCountryDetail(id){
     return function(dispatch) {
-        return axios.get(`http://localhost:3001/countries/${id}`)
+        return axios.get(API + `/countries/${id}`)
                 .then((res)=>{
                     dispatch({type:GET_COUNTRY_BY_ID, payload: res.data})  
                 })
@@ -45,7 +46,7 @@ export  function getCountryDetail(id){
 
 export function getAllCountries(){
     return function (dispatch){
-        return axios.get('http://localhost:3001/countries/')
+        return axios.get( API + '/countries/')
                 .then((res)=> {
                     dispatch({type: GET_ALL_COUNTRIES, payload: res.data })
                 })
@@ -58,7 +59,7 @@ export function getAllCountries(){
 export function postActivity(data){
     return async function(dispatch){
         try {
-            const res = await axios.post('http://localhost:3001/activities', data)
+            const res = await axios.post(API + '/activities', data)
             getActivities();
             return res
         } catch (error) {
@@ -77,7 +78,7 @@ export function setLoadingFalse(){
 
 export function getCountryByName(name){
     return function (dispatch){
-        return axios.get(`http://localhost:3001/countries?name=${name}`)
+        return axios.get(API + `/countries?name=${name}`)
                     .then((res)=>{
                         dispatch({type:GET_COUNTRY_BY_NAME, payload: {countries: res.data, name}})
                     })
@@ -93,7 +94,7 @@ export function setPageInState(fct, actualPage){
 
 export function getContinents(){
     return function (dispatch){
-        return axios.get('http://localhost:3001/continents')
+        return axios.get(API + '/continents')
                     .then((res)=>{
                         dispatch({type:GET_CONTINENTS, payload: res.data})
                        
